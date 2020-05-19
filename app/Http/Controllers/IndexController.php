3815ceb6996a99bf5ad1f2fa10133b7ad332d022
Sargis2020 <saqharutyunyan2018@gmail.com>
2020-05-19 16:10:20 +0300

    uxarkac

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,11 +12,12 @@ class IndexController extends Controller
          {
             return view('user.index')->with('logo', $logo);
           }
+          $logo['name'] = 'Your Teaste';
           $logo['type'] = 'text';
           $logo['call_name'] = 'Call Me';
           $logo['phone'] = '111111111';
           $logo['reserv_name'] = 'Reserve';
           return view('user.index')->with('logo', $logo);
-          $logo['name'] = 'Your Teaste';
+          
       } 
   }
